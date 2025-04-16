655db94ebe65e6a88b3bb78d272bfec1a48ae30b
Kirill <alex.ujikol@mail.ru>
2025-04-16 19:37:25 +0500

    update colors

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ function Main() {
                 <div className="project-settings">
                     <div className="project-header">
                         <h1>Alfaproject</h1>
-                        <button className="save-btn"><img src="../imgs/mark.svg"></img>Сохранить</button>
+                        <button className="save-btn"><img src="../imgs/save.svg"></img>Сохранить</button>
                         <button className="share-btn"><img src="../imgs/share.svg"></img>Поделиться</button>
                     </div>
                     <div className="filter">
